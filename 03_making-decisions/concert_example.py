9db193937eae7c7ab74869fc326e51c0cfa5eeef
Oliver Antoniu <oliver+github@abolish.golf>
2025-09-19 09:55:59 -0600

    03 - making decisions: multiple conditions example

---
+++ 03_making-decisions/concert_example.py
@@ -0,0 +1,14 @@
+concert_name = input("What is the name of the concert? ")
+has_ticket = input("Do you have a ticket? (y/n) ")
+
+if concert_name == "taylor swift" and has_ticket == "y":
+    print("you're in the right place")
+    print("have fun!")
+if concert_name == "taylor swift" and has_ticket == "n":
+    print("you need a ticket, bestie")
+elif concert_name == "billie eilish":
+    print("that concert is next door")
+elif concert_name == "ozzy reanimation tour":
+    print("R.I.P. dark prince")
+else:
+    print("this is not the concert you're looking for")
