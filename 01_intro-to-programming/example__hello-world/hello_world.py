3cd706de4bc769906abc0eb9c6223d10a8fc80ee
oaao <oliver+github@abolish.golf>
2025-09-21 14:08:24 -0600

    01 - intro to programming: Hello World example

---
+++ 01_intro-to-programming/example__hello-world/hello_world.py
@@ -0,0 +1,23 @@
+# This is a comment
+
+print("Hello from SDEV1001")
+# is there a difference with apostrophes
+# and quotations
+# python treats relatively the same
+# but it's better grammar to use double quotes
+
+print('Hello using them single quotes')
+
+# let's take a look at what an error looks like
+
+# print) # this is an invalid python line
+# because there's no matching bracket
+
+# example of an error below
+
+# $ python hello_world.py
+#   File "C:\Users\dmouris\lectures\sdev1001-Fall-2025-A02\1-hello-world-example-start\hello_world.py", line 13
+#     print)
+#          ^
+# SyntaxError: unmatched ')'
+# '''
