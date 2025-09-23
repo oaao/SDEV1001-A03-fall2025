faffd7ae6ed27169c570b2c459796942af0dbed5
Oliver Antoniu <oliver+github@abolish.golf>
2025-09-23 08:54:43 -0600

    04 - making decisions: hi/low, implement intermediary user input

--- 04_making-decisions/examples/02__high-or-low-game/high_or_low.py
+++ 04_making-decisions/examples/02__high-or-low-game/high_or_low.py
@@ -2,7 +2,7 @@ import random
 
 # get a random *integer*
 random_int = random.randint(1, 100)  # inclusive range
-print(f"Computer's random number is: {random_int}")
+print(f"(privileged information) Computer's random number is: {random_int}")
 
 
 # get user input
@@ -10,13 +10,17 @@ user_input = input("Guess a number between 1 and 100: ")
 user_guess = int(user_input)
 print(f"User guessed: {user_guess}")
 
+high_low_input = input("Do you think you are higher or lower than the number? (h/l): ")
+
 
 # compare computer's number w/ user's guess
+result = ""
+
 if user_guess == random_int:
-    print("Wow! You guessed the number!")
+    result = "correct"
 elif user_guess > random_int:
-    print("Your guess is higher than the number.")
+    result = "high"
 elif user_guess < random_int:
-    print("Your guess is lower than the number.")
+    result = "low"
 else:
-    print("Sorry Dave, couldn't compute that.")
+    result = "error"
