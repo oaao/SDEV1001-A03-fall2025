906cdcc100682e423d71e0dde898aeb843333d5e
Oliver Antoniu <oliver+github@abolish.golf>
2025-09-23 08:34:29 -0600

    04 - making decisions: hi/low example, finish getting user input

--- 04_making-decisions/examples/02__high-or-low-game/high_or_low.py
+++ 04_making-decisions/examples/02__high-or-low-game/high_or_low.py
@@ -2,6 +2,11 @@ import random
 
 # get a random *integer*
 random_int = random.randint(1, 100)  # inclusive range
-print(f"Our random number is: {random_int}")
+print(f"Computer's random number is: {random_int}")
 
+
+# get user input
+user_input = input("Guess a number between 1 and 100: ")
+user_guess = int(user_input)
+print(f"User guessed: {user_guess}")
 
