2e455c25ee37c1a12de4f644aa3458f661452c88
Oliver Antoniu <oliver+github@abolish.golf>
2025-09-23 08:46:45 -0600

    04 - making decisions: hi/low example, conditional logic

--- 04_making-decisions/examples/02__high-or-low-game/high_or_low.py
+++ 04_making-decisions/examples/02__high-or-low-game/high_or_low.py
@@ -10,3 +10,13 @@ user_input = input("Guess a number between 1 and 100: ")
 user_guess = int(user_input)
 print(f"User guessed: {user_guess}")
 
+
+# compare computer's number w/ user's guess
+if user_guess == random_int:
+    print("Wow! You guessed the number!")
+elif user_guess > random_int:
+    print("Your guess is higher than the number.")
+elif user_guess < random_int:
+    print("Your guess is lower than the number.")
+else:
+    print("Sorry Dave, couldn't compute that.")
