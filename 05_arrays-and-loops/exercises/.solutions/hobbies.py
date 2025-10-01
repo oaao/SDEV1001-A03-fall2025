1e8a5ec22de35cdf5e184db9d77264c74b50a513
Oliver Antoniu <oliver+github@abolish.golf>
2025-10-01 09:51:23 -0600

    add [05 - arrays and loops]: exercise 1 solution

---
+++ 05_arrays-and-loops/exercises/.solutions/hobbies.py
@@ -0,0 +1,17 @@
+hobbies = [
+    "playing squash",        # 0
+    "programming",           # 1
+    "reading",               # 2 ...
+    "petting cats",
+    "playing golf",
+    "visiting friends",
+    "playing video games",
+    "playing board games",
+    "watching movies"
+]
+
+
+print(f"length of list: {len(hobbies)}")
+print(f"FIFTH element in list: {hobbies[4]}")  # indices start at 0
+print(f"first four items: {hobbies[:4]}")      # 0, 1, 2, 3 // 4
+print(f"last three items: {hobbies[-3:]}")     # -3, -2, -1
