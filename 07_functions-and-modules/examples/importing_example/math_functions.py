44b1bbd1fc6427ed6d16d20853be337a8a3186db
Oliver Antoniu <oliver+github@abolish.golf>
2025-10-21 08:45:07 -0600

    add [07 - functions and modules]: importing example]

---
+++ 07_functions-and-modules/examples/importing_example/math_functions.py
@@ -0,0 +1,8 @@
+# math_functions.py
+
+def add(a, b):
+	return a + b
+
+
+def subtract(a, b):
+	return a - b
---
+++ 07_functions-and-modules/examples/program.py
@@ -0,0 +1,8 @@
+# program.py
+
+# I need an __init__.py in any directory I'm importing *from*,
+# so that Python ingests it as a Python module
+
+from importing_example import math_functions
+
+print(math_functions)
