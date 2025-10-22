ef097074719fa6f2eb213fb968dfd0018c072c37
Oliver Antoniu <oliver+github@abolish.golf>
2025-10-22 09:25:29 -0600

    rev [07 - functions and modules]: finish car price single-file example

--- 07_functions-and-modules/exercises/.solutions/03_car_price_guessing_game.py
+++ 07_functions-and-modules/exercises/.solutions/03_car_price_guessing_game.py
@@ -1,16 +1,48 @@
-def guess_car_price(guess, price=20000):
+"""
+keep in mind that the variables {msg} and {guess} here are globally scoped in this file,
+whereas {guess} and {msg} inside guess_car_price() are only accessible within that function's scope
+--> this is also why those names/values don't conflict
+"""
+
+import random
+
+
+def guess_car_price(guess: int) -> str:  # python type hinting: https://docs.python.org/3/library/typing.html
+
+	price = random.randint(5000, 25000)
+	print(f"DEBUG: price is {price}")
+
 	if guess == price:
 		msg = "You win; that's exactly the price! You're a cheater!"
 	elif abs(price - guess) <= 1000:  # abs() -> absolute value, i.e. abs(-5) -> 5
 		msg = "You win!"
-	# ... to be continued..
+	# we don't have to explicitly check for: 1000 < abs(price - guess) <= 5000,
+	# because of code flow!
+	# if the program even gets to this conditional block, we already know it's > 1000
+	elif abs(price - guess) <= 5000:
+		msg = "You're close!"
+	else:
+		msg = "Sorry, you lose!"
+
+	return msg
 
 
 
-# Perhaps a better way of controlling code flow here would be:
 playing = True  # state variable - controls/affects the state of the program
 
 while playing:
-	guess = input("Guess or {n}")
+
+	guess = input(
+		"\nGuess the price of the car (between 5,000 and 25,000), or {n} to quit: "
+	)
 	if guess == "n":
 		playing = False
+		print("Bye, coward!")
+		quit()  # break works here too, if you just want to exit the while loop and not the whole program
+
+	try:
+		guess = int(guess)  # I expect the ValueError to occur on this line
+		msg = guess_car_price(guess)
+		print(msg)
+	except ValueError:
+		print("Please make sure your guess is a whole number.")
