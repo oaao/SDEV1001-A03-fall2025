d28518184fe12c59e0f52e8f0b09a9a0229259c2
Oliver Antoniu <oliver+github@abolish.golf>
2025-10-22 08:34:47 -0600

    rev [07 - functions and modules]: use state variable to control game flow

--- 07_functions-and-modules/exercises/.solutions/03_car_price_guessing_game.py
+++ 07_functions-and-modules/exercises/.solutions/03_car_price_guessing_game.py
@@ -1,7 +1,3 @@
-# PARTIALLY COMPLETED!
-
-guess = None  # I could also start this off as a blank string "", but this is what 'null' looks like in Python
-
 def guess_car_price(guess, price=20000):
 	if guess == price:
 		msg = "You win; that's exactly the price! You're a cheater!"
@@ -10,7 +6,11 @@ def guess_car_price(guess, price=20000):
 	# ... to be continued..
 
 
-while guess != "n":
-	guess = input("Guess the price of the car, or {n} to exit: $")
 
+# Perhaps a better way of controlling code flow here would be:
+playing = True  # state variable - controls/affects the state of the program
 
+while playing:
+	guess = input("Guess or {n}")
+	if guess == "n":
+		playing = False
