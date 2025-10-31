8efe96abb1ba27693e345e39634c257fdd7e7e61
Oliver Antoniu <oliver+github@abolish.golf>
2025-10-31 09:33:49 -0600

    rev [08 - dictionaries]: basic example, more access methods

--- 08_dictionaries/examples/dictionaries.py
+++ 08_dictionaries/examples/dictionaries.py
@@ -15,7 +15,7 @@ print(
 
 
 # multiple values for the same key will retain the most recent value
-d2 {
+d2 = {
 	"name": "Oliver",
 	"age": 35,
 	"age": 12,
@@ -40,3 +40,25 @@ print(d.items())  # gives us a [(key, val), ... ] pairwise structure
 # because the structure is pairwise, we iterate over it with *two* placeholder terms
 for key, value in d.items():
 	print(f"{key}: {value}")
+
+
+# if I try to access a nonexistent key, e.g. d["hobby"], I'll get a KeyError
+try:
+	key = "key that does not exist"
+	d[key]
+except KeyError:
+	print(f"{key} is not a valid key in:\n{d}")
+
+# however, if I use the .get() on a dictionary, I just get None if the key is missing
+#     basically, d[k] vs. d.get(k)
+#     is a decision between whether you want to trigger an exception, or return a default value (None),
+#     if trying to access a missing key.
+print(
+	d.get("nonexistent key")
+	# this is like, d.get("nonexistent key", default=None)
+)
+
+# you can also *dictate* what the default is rather than None
+print(
+	d.get("nonexistent key", "default value")
+)
