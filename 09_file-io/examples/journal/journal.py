e824e2a77d6cfe34561cf6c84ab107f5fe56a9de
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-12 08:24:55 -0700

    rev [09 - file io]: journal example - refactor into menu text inputs w/ looping

--- 09_file-io/examples/journal/journal.py
+++ 09_file-io/examples/journal/journal.py
@@ -18,8 +18,23 @@ def write_journal(filename, journal_entry):
 if __name__ == "__main__":
 	
 	file_path = "journal.txt"
-	entries = read_journal(file_path)
 
-	print(f"\nJournal entries in {file_path}:")
-	for i, entry in enumerate(entries, start=1):
-		print(f"{i}: {entry}")
+	while True:
+		
+		action = input("\nDo you want to:\n  (r)ead the journal, (w)rite a new entry, or (q)uit? ").lower()
+
+		match action:
+			case "r":
+				entries = read_journal(file_path)
+				print(f"\nJournal entries in {file_path}:\n")
+				for i, entry in enumerate(entries, start=1):
+					print(f"{i}: {entry}")
+			case "w":
+				new_entry = input("Enter your journal entry:\n")
+				write_journal(file_path, new_entry)
+				print("Entry added to journal.")
+			case "q":
+				print("OK, see ya!")
+				break
+			case _:
+				print(f"{action} is not a valid action. Please try again.")
