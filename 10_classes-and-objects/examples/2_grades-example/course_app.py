ec1303d1f60f631f6101c54d6307e7330089812e
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-19 09:39:46 -0700

    add [10 - classes & objects]: app example 2, basic program for the app

---
+++ 10_classes-and-objects/examples/2_grades-example/course_app.py
@@ -0,0 +1,9 @@
+from data.course_data import assignment_data, student_data, submission_data
+
+from tools.course import Course
+
+
+if __name__ == "__main__":
+
+	 course = Course("Colourful Entomology 304")
+	 print(course)
