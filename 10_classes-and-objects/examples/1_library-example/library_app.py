45b18124d24e4c6acb20f7f5fc5b76a5a582920c
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-19 09:17:48 -0700

    rev [10 - classes & object]: app example 1, library: add/list books method & implementation

--- 10_classes-and-objects/examples/1_library-example/library_app.py
+++ 10_classes-and-objects/examples/1_library-example/library_app.py
@@ -7,11 +7,19 @@ if __name__ == '__main__':
     print("Welcome to our Library App")
     print("--------------------------")
 
-    # step 6: create a library
     library = Library("Edmonton Public Library")
     print(library)  # -> "Edmonton Public Library"
 
+    # first, list the books before we've added any
+    library.list_books()  # -> "Library is empty!"
+
     book = Book("The Lord of the Rings", "J.R.R. Tolkien", 1000)
+    bookTwo = Book("The Wheel of Time", "Robert Jordan", 690)
+    bookThree = Book("The Way of Kings", "Brandon Sanderson", 1200)
+    bookFour = Book("Mistborn", "Brandon Sanderson", 640)
 
+    books_to_add = (book, bookTwo, bookThree, bookFour)  # just for easy looping
+    for b in books_to_add:
+        library.add_book(b)
 
-
+    library.list_books()
--- 10_classes-and-objects/examples/1_library-example/library_tools/library.py
+++ 10_classes-and-objects/examples/1_library-example/library_tools/library.py
@@ -6,3 +6,15 @@ class Library:
 
 	def __str__(self):
 		return self.name
+
+	def add_book(self, book):
+		self.books.append(book)
+
+	def list_books(self):
+		if not self.books:  # or, if len(self.books) == 0:
+			print("Library is empty!")
+		else:
+			print(f"\nBooks currently in {self}:")
+			for book in self.books:
+				print(f"  - {book}")
+
