4292c5a19f4721cafdfe88fd4e7874bf836afebc
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-19 08:51:10 -0700

    rev [10 - classes & objects]: app example 1: library, fixes & imports

--- 10_classes-and-objects/examples/1_library-example/library_app.py
+++ 10_classes-and-objects/examples/1_library-example/library_app.py
@@ -1,3 +1,19 @@
+from library_tools.book import Book
+
+
 if __name__ == '__main__':
+    
     print("Welcome to our Library App")
     print("--------------------------")
+
+    book = Book("The Lord of the Rings", "J.R.R. Tolkien", 1000)
+
+    # let's print the books properties.
+    print("The properties of our book are:")
+    print(f"Title: {book.title}")
+    print(f"Author: {book.author}")
+
+    # let's print the book.
+    print("The book is:")
+    print(book)              # this calls Book.__str__()
+
--- 10_classes-and-objects/examples/1_library-example/library_tools/Book.py
+++ 10_classes-and-objects/examples/1_library-example/library_tools/Book.py
@@ -1,5 +1,5 @@
 class Book:
 	def __init__(self, title, author, pages):
-		self.title = title,
-		self.author = author,
+		self.title = title
+		self.author = author
 		self.pages = pages
