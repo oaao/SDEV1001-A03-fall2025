e21142b4d414c699f54d3a43d32c452815a972e9
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-21 08:49:02 -0700

    rev [10 - classes & objects]: app example 2, course - 'getter' methods on Course

--- 10_classes-and-objects/examples/2_grades-example/models/course.py
+++ 10_classes-and-objects/examples/2_grades-example/models/course.py
@@ -7,9 +7,23 @@ class Course:
     def __str__(self):
         return f"{self.name} has {len(self.students)} students and {len(self.assignments)} assignments"
 
+    # 'writing' to attributes
     def add_student(self, student):
         self.students.append(student)
 
     def add_assignment(self, assignment):
         self.assignments.append(assignment)
 
+
+    # 'reading from' attributes
+    def get_student(self, student_id):
+        for student in self.students:
+            if student.id == student_id:
+                return student
+        return None
+
+    def get_assignment(self, assignment_id):
+        for assignment in self.assignments:
+            if assignment.id == assignment_id:
+                return assignment
+        return None
