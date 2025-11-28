e5c7695eded2f1e7568a7c7453ce747fd1938ae9
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-28 09:40:38 -0700

    fix [11 - packages]: slide examples - only run example printout if __main__ on report_config

--- 11_packages/examples/0_slide-examples/report_config.py
+++ 11_packages/examples/0_slide-examples/report_config.py
@@ -16,18 +16,18 @@ def get_config():
     }
 
 
-
-# 1. I'm going to run get_config() before loading them in
-print(get_config())
-
-
-# 2. I'm going to load those environment variables using
-# dotenv.load_dotenv(), part of the dotenv package
-load_dotenv()  
-print("dotenv.load_dotenv() called...")
-# it looks for an .env file in the same directory, and loads all environment variables
-# into the runtime of this program!
+if __name__ == "__main__":
+    # 1. I'm going to run get_config() before loading them in
+    print(get_config())
 
 
-# 3. Call our get_config() function again to check
-print(get_config())
+    # 2. I'm going to load those environment variables using
+    # dotenv.load_dotenv(), part of the dotenv package
+    load_dotenv()  
+    print("dotenv.load_dotenv() called...")
+    # it looks for an .env file in the same directory, and loads all environment variables
+    # into the runtime of this program!
+
+
+    # 3. Call our get_config() function again to check
+    print(get_config())
