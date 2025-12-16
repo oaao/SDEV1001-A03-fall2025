f448a55c91f72cd421ca20561430af0ce4fe557b
Oliver Antoniu <oliver+github@abolish.golf>
2025-12-16 09:21:02 -0700

    rev [12 - aoc]: day 1A, two ways of input parsing

--- 12_advent-of-code/day-01/01.py
+++ 12_advent-of-code/day-01/01.py
@@ -13,6 +13,17 @@ with open(filename, 'r') as f:
 	# 1. when I open a file & read from it, I get strings...
 	# 2. I want to replace "L" with -, and "R" with nothing
 	# 3. I want to cast those into integers
+	# 4. If I can write a for loop, I can write a list expression!
+	#   The easy mental cheat code for writing a list expression is..
+    #   ... take the end-term in the for loop, put it at the start, write the rest
+	dial = [
+		int(
+			line.replace("L", "-").replace("R", "")
+		)
+		for line in f.readlines()
+	]
+
+
 	dial = []
 	for line in f.readlines():
 		line = line.replace("L", "-").replace("R", "")
