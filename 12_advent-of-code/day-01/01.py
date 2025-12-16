4f89e14eb7e7d0a1fe0b34cff77e354adab85cd5
Oliver Antoniu <oliver+github@abolish.golf>
2025-12-16 09:43:38 -0700

    rev [12 - aoc]: 1A, solution

--- 12_advent-of-code/day-01/01.py
+++ 12_advent-of-code/day-01/01.py
@@ -2,13 +2,13 @@
 EXERCISE PROMPT: http://adventofcode.com/2025/day/1
 """
 
-START_POS = 50
+INPUT_FILE = "sample"
 
 # option 1: good ol' for loop
 # option 2: make a list [0] * 100 & +=1 each index landed at
-# option 3: 
+# option 3: i don't have to resolve the range at all! i just need... x % 100 = 0
 
-with open(filename, 'r') as f:
+with open(INPUT_FILE, 'r') as f:
 	# what do I need to do for input parsing?
 	# 1. when I open a file & read from it, I get strings...
 	# 2. I want to replace "L" with -, and "R" with nothing
@@ -17,14 +17,22 @@ with open(filename, 'r') as f:
 	#   The easy mental cheat code for writing a list expression is..
     #   ... take the end-term in the for loop, put it at the start, write the rest
 	dial = [
-		int(
-			line.replace("L", "-").replace("R", "")
-		)
+		int(line.replace("L", "-").replace("R", ""))
 		for line in f.readlines()
 	]
 
 
-	dial = []
-	for line in f.readlines():
-		line = line.replace("L", "-").replace("R", "")
-		dial.append(int(line))
+position = 50  # dial starts at 50
+zeros = 0
+# option 3: we just care about positions we land on
+#           that are fully divisible by 100
+for instruction in dial:
+	# print(f"Starting at {position},")
+	position += instruction
+	# print(f"Updating position to {position} (with step: {instruction}")
+	if position % 100 == 0:
+		print("Landed on a zero!")
+		zeros += 1
+
+print(f"part A solution: {zeros}")
+
